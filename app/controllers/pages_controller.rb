73be7611b6677dc8bdfb7aec6e20cb97fcf5b165
Joe Knowles <joejknowles@gmail.com>
2024-09-23 12:54:56 +0100

    Separate pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,32 +3,35 @@ require "httparty"
 class PagesController < ApplicationController
   def index
     token_response = fetch_token
+    @zones = fetch_zones(token_response["access_token"]) if token_response["access_token"]
+    @zones.each do |zone|
+      date_zone_created = zone["dateCreated"].to_date
+      yesterday = Date.yesterday
+      zone_yesterday = ZoneReport.where(zone_id: zone["id"], requested_date: yesterday).first
+      if zone_yesterday.present?
+        zone_yesterday.requested_date
+      else
+        latest_existing = ZoneReport.where(zone_id: zone["id"]).order(requested_date: :desc).limit(1).first
+        starting_date = latest_existing ? latest_existing.requested_date + 1.day : date_zone_created
+        end_date = [ starting_date + 10.weeks, yesterday ].min
+        (starting_date..end_date).each do |date|
+          report = fetch_zone_day_report(token_response["access_token"], zone["id"], date)
+          new_report = ZoneReport.create(zone_id: zone["id"], requested_date: date, interval_from: report["interval"]["from"], interval_to: report["interval"]["to"], data: report)
+          p "Fetched report for #{zone["name"]} on #{date}"
+          sleep(0.5)
+          new_report.requested_date
+        end
+      end
+    end
+  end
+
+  def current_states
+    token_response = fetch_token
     @home = fetch_home(token_response["access_token"]) if token_response["access_token"]
     @zones = fetch_zones(token_response["access_token"]) if token_response["access_token"]
     @zone_states = @zones.map do |zone|
       response = HTTParty.get("https://my.tado.com/api/v2/homes/#{ENV["TADASH_MY_HOME_ID"]}/zones/#{zone["id"]}/state", headers: { "Authorization" => "Bearer #{token_response["access_token"]}" })
       JSON.parse(response.body)
-    end
-
-    @zone_history = @zones.map do |zone|
-      date_zone_created = zone["dateCreated"].to_date
-      today = Date.today
-      zone_today = ZoneReport.where(zone_id: zone["id"], requested_date: today).first
-      if zone_today.present?
-        zone_today.requested_date
-      else
-        latest_existing = ZoneReport.where(zone_id: zone["id"]).order(requested_date: :desc).limit(1).first
-        starting_date = latest_existing ? latest_existing.requested_date + 1.day : date_zone_created
-        end_date = [ starting_date + 10.weeks, today ].min
-        (starting_date..end_date).map do |date|
-          report = fetch_zone_day_report(token_response["access_token"], zone["id"], date)
-          p report
-          new_report = ZoneReport.create(zone_id: zone["id"], requested_date: date, interval_from: report["interval"]["from"], interval_to: report["interval"]["to"], data: report)
-          p "Fetched report for #{zone["name"]} on #{date}"
-          sleep(1)
-          new_report.requested_date
-        end
-      end
     end
   end
 
